ef276bcbdf878ffe808a6f5e57751cbe15af71db
yangfeiffei <yangfeilong_2009@126.com>
2019-09-03 21:38:39 +0800

    add write homework at 2019-09-03-21-38-39

--- scripts/git-add-and-push-jpg-in-write-homework.sh
+++ scripts/git-add-and-push-jpg-in-write-homework.sh
@@ -1,5 +1,13 @@
-sips -Z 1000 images/write-homework/*.jpg
 
-git add .
-git commit -m `date "+%Y-%m-%d-%H-%M-%S"`
-git push origin master
+# change jpg to 1000 width
+sips -Z 1000 images/write-homework/*.jpg
+if [ $? == 0 ]; 
+then
+	# add all jpg and push to master
+	git add . &&
+	COMMIT_DATE=$(date "+%Y-%m-%d-%H-%M-%S") &&
+	git commit -m "add write homework at $COMMIT_DATE"  &&
+	git push origin master ;
+else
+	echo "something wrong about change images.\nPlese check it..." ;
+fi
